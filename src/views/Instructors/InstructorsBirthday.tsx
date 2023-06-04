8356d9f9979822f738eb92252053addf2477e36c
bdnrJ <jbednarczyk001@gmail.com>
2023-06-04 21:32:52 +0200

    dasdas

--- src/views/Instructors/InstructorsBirthday.tsx
+++ src/views/Instructors/InstructorsBirthday.tsx
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import Popup from '../../components/Popup'
-import AddInstructor from '../../Formy/AddInstructor'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Popup from '../../components/Popup';
+import AddInstructor from '../../Formy/AddInstructor';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Instructors = () => {
-
   const [instructors, setInstructors] = useState([]);
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   useEffect(() => {
-    const fetchStudents = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/api/instructors/birthday/10');
-        setInstructors(response.data);
-        console.log(response);
-      } catch (err) {
-        console.error('Error fetching students:', err);
-      }
-    };
+    if (selectedMonth !== "") {
+      const fetchInstructors = async () => {
+        try {
+          const response = await axios.get(`http://localhost:8080/api/instructors/birthday/${selectedMonth}`);
+          setInstructors(response.data);
+          console.log(response);
+        } catch (err) {
+          console.error('Error fetching instructors:', err);
+        }
+      };
+      fetchInstructors();
+    }
+  }, [selectedMonth]);
 
-    fetchStudents();
-  }, []);
+  const handleMonthSelect = (event) => {
+    setSelectedMonth(months.indexOf(event.target.value) + 1);
+  };
 
   return (
     <div>
       <section className="min-h-screen bg-slate-800 font-sans text-center">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-
-        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700">
           <span className="sr-only">Open sidebar</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
@@ -48,7 +53,6 @@ const Instructors = () => {
                   <span className="ml-3">Instructors</span>
                 </Link>
               </li>
-
               <li>
                 <Link to="/InstructorsBirthday" className="flex items-center p-2 text-base font-normal text-blue-700 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg aria-hidden="true" className="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -58,7 +62,6 @@ const Instructors = () => {
                   <span className="ml-3">InstructorsBirthday</span>
                 </Link>
               </li>
-
               <li>
                 <Link to="/InstructorsMaxLesson" className="flex items-center p-2 text-base font-normal text-blue-700 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg aria-hidden="true" className="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -68,16 +71,18 @@ const Instructors = () => {
                   <span className="ml-3">InstructorsMaxLesson</span>
                 </Link>
               </li>
-
             </ul>
-
           </div>
-
         </aside>
-
         <div className="p-4 sm:ml-64">
           <div className="mx-auto flex-1 p-3">
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <p className="text-lg text-white">Please select a month to show the instructors' birthdays:</p>
+            <select onChange={handleMonthSelect} value={selectedMonth} className="text-white text-lg bg-gray-800">
+              <option>Select a month...</option>
+              {months.map((month, index) =>
+                <option key={index} value={month} className="text-white text-lg bg-gray-800">{month}</option>
+              )}
+            </select>
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -102,18 +107,11 @@ const Instructors = () => {
                   ))}
                 </tbody>
               </table>
-            </div>
-
           </div>
         </div>
-
-
       </section>
     </div>
+  );
+};
 
-
-
-  )
-}
-
-export default Instructors
+export default Instructors;
