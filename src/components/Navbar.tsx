518672e8f8a4b10a5947db53b94e95d6805c9e41
Damian <damdam6680@gmail.com>
2023-06-04 16:44:39 +0200

    Changes to be committed: 	modified:   src/components/Navbar.tsx 	modified:   src/views/Courser/CourseTodayLesson.tsx 	modified:   src/views/Courser/Courses.tsx 	modified:   src/views/Courser/CoursesMaxLesson.tsx 	modified:   src/views/Instructors/InstructorsMaxLesson.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,9 @@ const Navbar = () => {
 
     <nav className="bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a  className="flex items-center">
           <img src="src\assets\logo.png" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Skill Wheel</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
