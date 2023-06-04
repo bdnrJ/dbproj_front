335488c0139ae8cd88b929527a7464a9f677c9ac
cyboranf <cyboranf@gmail.com>
2023-06-04 22:40:00 +0200

    video to ReadME -added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,7 @@ const Footer = () => {
 <footer className="bg-white shadow dark:bg-gray-900 ">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
+        
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
